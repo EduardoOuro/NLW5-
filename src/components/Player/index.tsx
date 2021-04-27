e95a4e02614bf918854c43c72406aebd40e2e6d9
Luis Conrado <luis.conrado@VALEMOBI.CORP@VMDESK-76.valemobi.corp>
2021-04-26 21:15:21 -0300

    corrigindo nome da conversão de data para string

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -7,7 +7,7 @@ import 'rc-slider/assets/index.css';
 import { usePlayer } from '../../contexts/PlayerContext';
 
 import styles from './styles.module.scss';
-import { converDurationToTimeString } from '../../utils/convertDurationToTimeString';
+import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 
 export function Player() {
   const audioRef = useRef<HTMLAudioElement>(null);
@@ -88,7 +88,7 @@ export function Player() {
 
       <footer className={!episode ? styles.empty : ''}>
         <div className={styles.progress}>
-          <span>{converDurationToTimeString(progress)}</span>
+          <span>{convertDurationToTimeString(progress)}</span>
           <div className={styles.slider}>
             { episode ? (
               <Slider 
@@ -103,7 +103,7 @@ export function Player() {
               <div className={styles.emptySlider} />
             )}
           </div>
-          <span>{converDurationToTimeString(episode?.duration ?? 0)}</span>
+          <span>{convertDurationToTimeString(episode?.duration ?? 0)}</span>
         </div>
 
         { episode && (
